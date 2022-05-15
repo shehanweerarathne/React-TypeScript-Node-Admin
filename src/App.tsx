7a58c10d70cdb7f33b13a43949cc6a50dcbcf492
Chamith Shehan Weerarathna <66713045+shehanweerarathne@users.noreply.github.com>
2022-05-15 21:14:09 +0530

    Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import Menu from "./components/Menu";
+import Users from "./pages/Users/Users";
+import {BrowserRouter, Route,Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+            <Nav/>
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu/>
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+                            <Routes>
+                                <Route path="/" element={<Dashboard/>} />
+                                <Route path="users" element={<Users/>} />
+                            </Routes>
+
+                    </main>
+                </div>
+            </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
